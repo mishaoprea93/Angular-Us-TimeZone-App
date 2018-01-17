9357be81377cccbf1f662cf9b342c641ec92f6d2
Mihail Oprea <mishanea@Mihails-MacBook-Pro.local>
2018-01-17 10:44:04 -0800

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  today=new Date();
+  title:string="Us Time Zone Display"
+  zone='UTC';
+  onClick(new_zone){
+    if(new_zone=="clear"){
+      this.zone='UTC';
+    }else{
+      this.zone=new_zone;
+    }
+}
 }
